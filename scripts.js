c16b905fa53c07a89266179871dfaccd55215396
nina.ruizandres@gmail.com <Atento02>
2020-03-26 18:50:39 +0100

    Now the button works

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
 
 const inpt = document.getElementById('text-input');
 const number = document.getElementsByClassName('number');
-let button;
+const action = document.getElementsByClassName('action');
 
 for (let numb of number) {
     numb.addEventListener('click', function () {
         this.innerHTML;
-        inpt.value = this.innerHTML;
+        inpt.value = "" + inpt.value + this.innerHTML;
     });
 }
 
 
+for (let act of action) {
+    act.addEventListener('click', function () {
+        this.innerHTML;
+        inpt.value = "" + inpt.value + this.innerHTML;
+    });
+}
 
