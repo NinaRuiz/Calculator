b65d4d1da9e651c7042cacf60e05940602858a40
nina.ruizandres@gmail.com <Atento02>
2020-03-26 18:40:43 +0100

    Some litte changes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 
-document.
+const inpt = document.getElementById('text-input');
+const number = document.getElementsByClassName('number');
+let button;
+
+for (let numb of number) {
+    numb.addEventListener('click', function () {
+        this.innerHTML;
+        inpt.value = this.innerHTML;
+    });
+}
+
+
+
